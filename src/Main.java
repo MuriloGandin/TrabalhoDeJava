05d5c63681d2698122f1a9afdaaf6da8d5644b35
Clara <clarapujoldecarvalho@gmail.com>
2026-05-19 22:16:53 -0300

    Atualizado Main e adicionado Sistema

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import View.Sistema;
+
 public class Main {
     public static void main(String[] args) {
         // trabalho java  tema: RPG
-        System.out.println("Hello World!");
+        Sistema.executar();
     }
 }
