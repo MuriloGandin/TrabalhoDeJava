232ca76c1a11fb1870809059fb94884a846e3463
Erick <erickmendes448@gmail.com>
2026-05-19 21:59:22 -0300

    Colocando o metodo receberDano

--- src/Model/Entidade.java
+++ src/Model/Entidade.java
@@ -1,4 +1,19 @@
 package Model;
 
 public class Entidade {
+
+    private int pontosDeVida;
+    boolean estaVivo;
+
+    public Entidade(int pontosDeVida, boolean estaVivo) {
+        this.pontosDeVida = pontosDeVida;
+        this.estaVivo = estaVivo;
+    }
+
+    public void receberDano(int dano){
+        pontosDeVida -= dano;
+        if (pontosDeVida < 1){
+            System.out.println("Inimigo Derrotado");
+        }
+    }
 }
