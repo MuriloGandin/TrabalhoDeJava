120918f5d516d058333bbf72e9679bc2a5a49e6e
murilo <muriloandradegandin@gmail.com>
2026-05-22 21:13:41 -0300

    menu principal

--- src/View/Sistema.java
+++ src/View/Sistema.java
@@ -2,6 +2,12 @@ package View;
 
 public class Sistema {
     public static void executar(){
+        mostrarMenuInicial();
+        InputHelper.scan.nextLine();
+    }
 
+    public static void mostrarMenuInicial() {
+        System.out.println("----- NullPointer Quest -----");
+        System.out.println("-- Pressione ENTER para jogar --");
     }
 }
