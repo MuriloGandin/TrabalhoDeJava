b6555af92dc3da57e10c0f238e300f6b7131f610
murilo <muriloandradegandin@gmail.com>
2026-05-22 22:05:47 -0300

    teste de combate

--- src/View/Sistema.java
+++ src/View/Sistema.java
@@ -1,9 +1,27 @@
 package View;
 
+import Model.Inimigo;
+import Model.Personagem;
+
 public class Sistema {
     public static void executar(){
         mostrarMenuInicial();
         InputHelper.scan.nextLine();
+
+        Inimigo creeper = new Inimigo(12, true);
+        creeper.setDano(5);
+        creeper.setNome("Creeper");
+
+        Personagem steve = new Personagem(20, true);
+        steve.setDano(6);
+        steve.setNome("Steve");
+
+        System.out.println("Vida do creeper: " + creeper.getPontosDeVida());
+        System.out.println("Pressione enter para atacar");
+        InputHelper.scan.nextLine();
+        steve.atacar(creeper);
+        System.out.println("Vida do creeper: " + creeper.getPontosDeVida());
+
     }
 
     public static void mostrarMenuInicial() {
