32f42f0b35fa8c2321c360219a277fca1fa9345d
CLARA RIBEIRO PUJOL DE CARVALHO <clara.carvalho@cs.up.edu.br>
2026-06-17 09:24:05 -0300

    mudanca pequena

---
+++ src/Controller/OndaController.java
@@ -0,0 +1,15 @@
+package Controller;
+
+import Model.Inimigo;
+import Model.Onda;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OndaController {
+
+
+}
